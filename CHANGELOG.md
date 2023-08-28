7ad2094edba44f2239a348a398adcdff750b2f7a
Eric Christensen <hebrew.8.12@gmail.com>
2023-08-27 21:29:06 -0700

    0.0.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## I wont be updating this changelog until the project hits 0.5
 
+## [0.0.11] - 2023-08-27
+
 ## [0.0.10] - 2023-08-27
 
 ## [0.0.9] - 2023-08-27
--- web/source/js/compiler.mjs
+++ web/source/js/compiler.mjs
@@ -1,5 +1,4 @@
 import { build } from "esbuild";
-import { solidPlugin } from "esbuild-plugin-solid";
 
 await build({
   entryPoints: ["web/source/js/scripts/navigation.js"],
@@ -8,5 +7,4 @@ await build({
   minify: true,
   sourcemap: false,
   outdir: "web/static/js",
-  plugins: [solidPlugin()],
 }).catch(() => process.exit(1));
